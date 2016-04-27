d58dc5ef7d14772b76afd6a676c17028f49fda65
Frederick Mfinanga <frederick.mfinanga@gmail.com>
2016-04-27 15:05:15 -0400

    cleaned up about page + discover component

--- app/components/discover/Discover.jsx
+++ app/components/discover/Discover.jsx
@@ -10,45 +10,17 @@ class Discover extends React.Component{
 	//Images used below are simply for testing purpose &
 	//will be replaced by fjmva photography
 	render() {
+		var images = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+		var imageElements = images.map(function(image){
+			return (
+				<img className='grid-item'
+					src={'../.././assets/images/discover/' + image + '.jpg'}/>
+		   	);
+		});
 		return (
 			<Masonry
                className={'discover-grid'}>
-               <img className="grid-item"
-				src="../.././assets/images/discover/1.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/2.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/3.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/4.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/5.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/6.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/7.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/8.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/9.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/10.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/11.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/12.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/13.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/14.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/15.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/16.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/17.jpg"/>
-				<img className="grid-item"
-				src="../.././assets/images/discover/18.jpg"/>
+               {imageElements}
            </Masonry>
 		);
 	}
