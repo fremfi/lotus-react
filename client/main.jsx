a6ef35128390e0fae2d1c31741672745bc614e6f
Frederick Mfinanga <frederick.mfinanga@gmail.com>
2016-05-06 12:21:58 -0400

    removed blog component

--- client/main.jsx
+++ client/main.jsx
@@ -5,9 +5,6 @@ import App from '../components/app';
 import Contact from '../components/pages/contact/Contact';
 import About from '../components/pages/about/About';
 import Discover from '../components/pages/discover/Discover';
-import Blog from '../components/pages/blog/Blog';
-// import { App } from '../components';
-// import { About, Discover, Blog, Contact } from '../components/pages';
 import { createStore, combineReducers } from 'redux';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import { Provider } from 'react-redux';
@@ -26,7 +23,6 @@ ReactDOM.render((
 	  	<Router history={history}>
 		    <Route path="/" component={App}>
 		    	<IndexRoute component={About}></IndexRoute>
-		      	<Route path="blog" component={Blog}></Route>
 		    	<Route path="contact" component={Contact}></Route>
 		    	<Route path="discover" component={Discover}></Route>
 		    </Route>
